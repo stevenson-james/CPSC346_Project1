1b2833e831ea4f6ad88c08d1f7441369fa56ae7c
James Stevenson <jpatstevenson@gmail.com>
2019-09-07 15:27:52 -0700

    changed variable names

--- main.c
+++ main.c
@@ -6,20 +6,18 @@ struct node{
     struct node* next;
 };
 
-void printList(struct node* head);
-
 //add to end of linked list
-void push(struct node** start, int value) {
-    if (*start == NULL){
-        *start = (struct node*) malloc(sizeof(struct node));
-        (*start)->next = NULL;
-        (*start)->data = value;
+void pushToEnd(struct node** head, int value) {
+    if (*head == NULL){
+        *head = (struct node*) malloc(sizeof(struct node));
+        (*head)->next = NULL;
+        (*head)->data = value;
     }
     else {
         struct node* newnode = (struct node*) malloc(sizeof(struct node));
         newnode->next = NULL;
         newnode->data = value;
-        struct node* temp = *start;
+        struct node* temp = *head;
         while (temp->next != NULL)
             temp = temp->next;
         temp->next = newnode;
@@ -27,64 +25,62 @@ void push(struct node** start, int value) {
 }
 
 struct node* merge(struct node* head1, struct node* head2){
-    struct node* newhead = NULL;
-    struct node* temp1 = head1;
-    struct node* temp2 = head2;
-    while(!(temp1 == NULL && temp2 == NULL))
+    struct node* newHead = NULL;
+    while(!(head1 == NULL && head2 == NULL))
     {
-        if(temp1 == NULL) {
-            push(&newhead, temp2->data);
-            temp2 = temp2->next;
+        if(head2 == NULL) {
+            pushToEnd(&newHead, head1->data);
+            head1 = head1->next;
         }
-        else if (temp2 == NULL) {
-            push(&newhead, temp1->data);
-            temp1 = temp1->next;
+        else if (head1 == NULL) {
+            pushToEnd(&newHead, head2->data);
+            head2 = head2->next;
         }
-        else if (temp1->data >= temp2->data){
-            push(&newhead, temp2->data);
-            temp2 = temp2->next;
+        else if (head1->data >= head2->data){
+            pushToEnd(&newHead, head2->data);
+            head2 = head2->next;
         }
         else {
-            push(&newhead, temp1->data);
-            temp1 = temp1->next;
+            pushToEnd(&newHead, head1->data);
+            head1 = head1->next;
         }
 
     }
-    return newhead;
+    return newHead;
 }
 
 int getSize(struct node* head){
     int size = 0;
-    while (head!=NULL)
-    {
+    while (head!=NULL) {
         head = head->next;
         size++;
     }
     return size;
 }
-void split (struct node* head, struct node** first, struct node** second, int mid){
+void split (struct node* head, struct node** secondHalf, int mid){
     for (int i = 1; i < mid; i++) {
         head = head->next;
     }
-    *second = head->next;
+    *secondHalf = head->next;
     head->next = NULL;
 }
 
 struct node* mergeSort(struct node* head, int size)
 {
-    struct node* first = head;
-    struct node* second = NULL;
+    struct node* firstHalf = head;
+    struct node* secondHalf = NULL;
     int mid = size / 2;
 
     if (size == 0 || size == 1) {
         return head;
     }
-    split(head, &first, &second, mid);
 
-    first = mergeSort(first, getSize(first));
-    second = mergeSort(second, getSize(second));
+    split(head, &secondHalf, mid);
 
-    return merge(first, second);
+    firstHalf = mergeSort(firstHalf, getSize(firstHalf));
+    secondHalf = mergeSort(secondHalf, getSize(secondHalf));
+
+    return merge(firstHalf, secondHalf);
 }
 void printList(struct node* head)
 {
@@ -96,20 +92,19 @@ void printList(struct node* head)
     printf("\n");
 }
 
-
 int main()
 {
     struct node* list1 = NULL;
     struct node* list2 = NULL;
-    struct node* mainlist = NULL;
-    push(&list1, 10);
-    push(&list1, 9);
-    push(&list2, 11);
-    push(&list2, 8);
-    push(&list2, 7);
-    mainlist = merge(list1, list2);
-    printList(mainlist);
-    mainlist = mergeSort(mainlist, getSize(mainlist));
-    printList(mainlist);
+    struct node* mergeList = NULL;
+    pushToEnd(&list1, 10);
+    pushToEnd(&list1, 9);
+    pushToEnd(&list2, 11);
+    pushToEnd(&list2, 8);
+    pushToEnd(&list2, 7);
+    mergeList = merge(list1, list2);
+    printList(mergeList);
+    mergeList = mergeSort(mergeList, getSize(mergeList));
+    printList(mergeList);
     return 0;
 }
